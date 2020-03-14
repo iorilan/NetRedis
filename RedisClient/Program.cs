98a6047d1337fc7b3c5b76ebd620e62ca0623bff
lanliang <iorilan@hotmail.com>
2020-03-14 22:37:44 +0800

    first

---
+++ RedisClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// A redis testing client 
+/// </summary>
+namespace RedisClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string ip = ConfigurationManager.AppSettings["Redis_Ip"];
+            var list = Enumerable.Range(0, 100).ToList();
+            var redis = new RedisLib.RedisClient(ip);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                redis.Set($"hello_{i}", $"{DateTime.Now.Ticks}_{i}", TimeSpan.FromSeconds(10));
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var v = redis.Get($"hello_{i}");
+                Console.WriteLine($"value :{v}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RedisLib/RedisClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace RedisLib
+{
+	public class RedisClient : IDisposable
+	{
+        private ConnectionMultiplexer _redisConnection;
+		private IDatabase _db;
+		public RedisClient(string host)
+		{
+			_redisConnection = ConnectionMultiplexer.Connect(host);
+			_db = _redisConnection.GetDatabase();
+		}
+
+		public bool Set(string key, string value, TimeSpan? expiry= null)
+		{
+			return _db.StringSet(key, value, expiry);
+		}
+
+		public string Get(string key)
+		{
+			var val = _db.StringGet(key);
+            if (val == RedisValue.Null || val == RedisValue.EmptyString)
+            {
+                return string.Empty;
+            }
+
+			return val.ToString();
+		}
+
+		public void Dispose()
+		{
+			_redisConnection.Dispose();
+		}
+	}
+}
